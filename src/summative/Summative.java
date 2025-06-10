f54e350247a1baedc9f11e0ebea97f68acde614b
lilya <lilya@10.127.50.28>
2025-06-10 01:02:56 -0400

    fire comes out of both suns now, death screen added, can now restart, next steps: include stages of multiple suns/levels, POSSIBLY custom sun images, plan out rest of story, health bar?

--- src/summative/Summative.java
+++ src/summative/Summative.java
@@ -15,6 +15,7 @@ public class Summative {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        MySketch.main("summative.MySketch");
+        
     }
-    
 }
